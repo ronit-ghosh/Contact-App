4c88619870179c23987e405107451a74a1dca8b1
ronit-ghosh <ronitghosh06@gmail.com>
2024-08-05 12:33:27 +0530

    Updated

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
-  apiKey: "REACT_APP_API_KEY",
-  authDomain: "REACT_APP_AUTH_DOMAIN",
-  projectId: "REACT_APP_PROD_ID",
-  storageBucket: "REACT_APP_STORAGE_BUCKET",
-  messagingSenderId: "REACT_APP_MSG_SENDER_ID",
-  appId: "REACT_APP_APP_ID"
+  apiKey: REACT_APP_API_KE ,
+  authDomain: REACT_APP_AUTH_DOMAI ,
+  projectId: REACT_APP_PROD_I ,
+  storageBucket: REACT_APP_STORAGE_BUCKE ,
+  messagingSenderId: REACT_APP_MSG_SENDER_I ,
+  appId: REACT_APP_APP_ID
 };
 
 export const app = initializeApp(firebaseConfig);

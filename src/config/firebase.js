befaded35682500aa0c6b64ff0ec557879040478
ronit-ghosh <ronitghosh06@gmail.com>
2024-08-05 12:22:14 +0530

    Updated

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,7 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
-  apiKey: "REACT_APP_API_KEY;",
+  apiKey: "REACT_APP_API_KEY",
   authDomain: "REACT_APP_AUTH_DOMAIN",
   projectId: "REACT_APP_PROD_ID",
   storageBucket: "REACT_APP_STORAGE_BUCKET",
